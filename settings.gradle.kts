3c711a061c455ada32205bea178e774f5d80bd73
felipdsa21 <93343201+felipdsa21@users.noreply.github.com>
2025-04-04 12:11:18 -0300

    Importado template

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "estudantes-backend"
---
+++ src/main/java/edge/academy/estudantes/EstudantesBackendApplication.java
@@ -0,0 +1,13 @@
+package edge.academy.estudantes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudantesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudantesBackendApplication.class, args);
+	}
+
+}
